f1e23038253229bd9d05e60775f730f58a8cda2a
Zubair <zubairsanaullah1122@gmail.com>
2026-03-30 10:11:13 +0500

    New

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,50 @@
 /*=============== HOME SPLIT TEXT ===============*/
+const { animate, text, stagger } = anime;
+
+const { chars: chars1 } = text.split('.home_profession-1', { chars: true })
+const { chars: chars2 } = text.split('.home_profession-2', { chars: true })
+
+animate(chars1, {
+    y: [
+        { to: ['100%', '0%'] },
+        { to: '-100%', delay: 4000, ease: 'in(3)' }
+    ],
+    duration: 900,
+    ease: 'out(3)',
+    delay: stagger(80),
+    loop: true,
+})
+
+animate(chars2, {
+    y: [
+        { to: ['100%', '0%'] },
+        { to: '-100%', delay: 4000, ease: 'in(3)' }
+    ],
+    duration: 900,
+    ease: 'out(3)',
+    delay: stagger(80),
+    loop: true,
+})
+
 
 
 /*=============== SWIPER PROJECTS ===============*/
+const swiperProjects = new Swiper('.projects_swiper', {
+    loop: true,
+    spaceBetween: 24,
+    slidesPerview: 'auto',
+    grabCursor: true,
+    speed: 600,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    }
+})
 
 
 /*=============== WORK TABS ===============*/
@@ -16,7 +59,7 @@
 /*=============== COPY EMAIL IN CONTACT ===============*/
 
 
-/*=============== CURRENT YEAR OF THE FOOTER ===============*/ 
+/*=============== CURRENT YEAR OF THE FOOTER ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
